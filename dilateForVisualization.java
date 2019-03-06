a1183819058b63d22004c0e6abeda9a7cee03ad1
Mark Olchanyi <46611964+mo1316@users.noreply.github.com>
2019-03-06 17:11:06 +0000

    Update dilateForVisualization.java

--- dilateForVisualization.java
+++ dilateForVisualization.java
@@ -26,20 +26,22 @@ public class dilateForVisualization {
 		 	ImageStack stack=image.getStack();
 		 	ImageStack stackxy_out=new ImageStack(stack.getWidth(),stack.getHeight());
 		 	
-		 	//for xy
+		 	//isolate slices
 		 	for (int i=1;i<=stack.getSize();i++)
 		 	{
 		 		ImageProcessor slice=stack.getProcessor(i);
 		 		ImagePlus temp_image=new ImagePlus("temp",slice);
 		 		Mat m=icc.convert(temp_image, Mat.class);		
 		 	      
-		 	      //dilate image to fill in edges
+		 	      //dilate image to fill in edges. Larger kernel will create better dilation but neuronal overlap error 
+			      //is also more likely to increase
 		 		final Size ksize = new Size(3, 3);       
 		 	    Mat kernel = opencv_imgproc.getStructuringElement(Imgproc.MORPH_CROSS, ksize);
-		 	    opencv_imgproc.dilate(m, m, kernel, new opencv_core.Point(-1,-1), 1, opencv_core.BORDER_CONSTANT, opencv_core.Scalar.BLACK); 
+		 	    opencv_imgproc.dilate(m, m, kernel, new opencv_core.Point(-1,-1), 1, opencv_core.BORDER_CONSTANT, opencv_core.Scalar.BLACK);
+			      //median blurring ensures that centers of branches are not left hollowed-out
 		 	    opencv_imgproc.medianBlur(m, m, 3);
 		 	      
-		 	    //now threshold again and turn into binary image (pixel values 255 or 0)
+		 	    //now threshold again and turn into binary image 
 		 	    opencv_imgproc.threshold(m, m, 0.7 * 1, 255, opencv_imgproc.CV_THRESH_BINARY);
 		 		
 		 		temp_image=mipp.convert(m, ImagePlus.class);
