e7df630da2a0b84e3d77a0b61c3e887f6e39cd9f
Mark Olchanyi <46611964+mo1316@users.noreply.github.com>
2019-03-07 11:27:44 +0000

    Update canny2d.java

--- canny2d.java
+++ canny2d.java
@@ -14,12 +14,14 @@ import ij.process.ImageProcessor;
 import ijopencv.ij.ImagePlusMatConverter;
 import ijopencv.opencv.MatImagePlusConverter;
 
+//use openCV to determine edges followed by filling with dilation and median filtering
 public class canny2d {
 	
 	public ImagePlus image;
 	public ImagePlus outputImage;
  	ImagePlusMatConverter icc = new ImagePlusMatConverter();
- 	MatImagePlusConverter mipp = new MatImagePlusConverter(); 
+ 	MatImagePlusConverter mipp = new MatImagePlusConverter();
+	//constructor requires only ImagePlus
  	public canny2d(ImagePlus imageInp) {
  		image = imageInp;
  	}
@@ -46,7 +48,7 @@ public class canny2d {
 	 	    Mat kernel = opencv_imgproc.getStructuringElement(Imgproc.MORPH_CROSS, ksize);
 	 	    opencv_imgproc.dilate(m, m, kernel, new opencv_core.Point(-1,-1), 1, opencv_core.BORDER_CONSTANT, opencv_core.Scalar.BLACK); 
 	 	    opencv_imgproc.medianBlur(m, m, 3);
-	 	    //opencv_imgproc.distanceTransform(m, m, opencv_imgproc.DIST_L1, 5);    	 	      
+	 	    opencv_imgproc.distanceTransform(m, m, opencv_imgproc.DIST_L1, 5);    	 	      
 	 	    //now threshold again and turn into binary image (pixel values 255 or 0)
 	 	    opencv_imgproc.threshold(m, m, 0.7 * 1, 255, opencv_imgproc.CV_THRESH_BINARY);
 	 		
